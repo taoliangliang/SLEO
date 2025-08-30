5464aefc379c8eea539d062baeff489015b90e85
taoliangliang <32632788+taoliangliang@users.noreply.github.com>
2025-08-30 10:31:03 +0800

    Update prob.py

--- prob.py
+++ prob.py
@@ -14,7 +14,6 @@ class Probs(object):
         self.unique_classes_ = unique_classes_
 
     def knn_probs(self):
-        # 寻找最近邻（不包括自身）
         probs = []
         IR = int((len(self.X) - len(self.minority_points)) / len(self.minority_points))
         if IR <= 5:
@@ -24,51 +23,47 @@ class Probs(object):
         nbrs = NearestNeighbors(n_neighbors=self.k + 1).fit(self.X)
         distances, indices = nbrs.kneighbors(self.minority_points)
         for i in range(len(self.minority_points)):
-            neighbor_indices = indices[i][1:]  # 排除自身
+            neighbor_indices = indices[i][1:]  
             neighbor_labels = self.y[neighbor_indices]
             counts = np.bincount(neighbor_labels, minlength=2)
             probs.append(counts / counts.sum())
         return np.array(probs)
 
     def logistic_probs(self):
-        #随机森林
         Logistic = LogisticRegression()
         Logistic.fit(self.X, self.y)
-        probs = Logistic.predict_proba(self.minority_points)  # 每个样本的类别概率分布
+        probs = Logistic.predict_proba(self.minority_points)
         return probs
 
     def svm_probs(self):
-        #支持向量基
-        svm = SVC(probability=True)  # 开启 Platt 缩放
+        svm = SVC(probability=True) 
         svm.fit(self.X, self.y)
-        probs = svm.predict_proba(self.minority_points)  # 每个样本的类别概率分布
+        probs = svm.predict_proba(self.minority_points) 
         return probs
 
     def bagging_probs(self):
-        # 定义 BaggingClassifier（基分类器支持概率输出）
         bagging = BaggingClassifier(estimator=DecisionTreeClassifier(), n_estimators=20)
         bagging.fit(self.X, self.y)
-        probs = bagging.predict_proba(self.minority_points)  # 每个样本的类别概率分布
+        probs = bagging.predict_proba(self.minority_points) 
         return probs
 
     def kde_probs(self):
         probs = []
         kde_models = {}
-        # 1. 为每个类别拟合一个 KDE 模型
         for cls in self.unique_classes_:
             X_cls = self.X[self.y == cls]
             kde = KernelDensity(bandwidth=1.0)
             kde.fit(X_cls)
             kde_models[cls] = kde
-        # 2. 对每个样本，计算它在各类别 KDE 中的概率密度
         for i in range(len(self.minority_points)):
             log_probs = []
             for cls in self.unique_classes_:
                 log_prob = kde_models[cls].score_samples(self.minority_points[i].reshape(1, -1))[0]  # log density
                 log_probs.append(log_prob)
             prob_arr = np.array(log_probs)
-            prob_arr = np.exp(prob_arr)  # 转换为密度
-            prob_arr /= prob_arr.sum()  # 归一化为“概率分布”
+            prob_arr = np.exp(prob_arr)  
+            prob_arr /= prob_arr.sum()  
             probs.append(prob_arr)
         probs = np.array(probs)
+
         return probs
