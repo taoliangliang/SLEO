b74b686a9a1eead7054b759f837aaee1b671be25
taoliangliang <32632788+taoliangliang@users.noreply.github.com>
2025-08-30 10:30:13 +0800

    Update LazyDistanceMatrix.py

--- LazyDistanceMatrix.py
+++ LazyDistanceMatrix.py
@@ -4,18 +4,18 @@ from scipy.spatial.distance import minkowski
 
 class LazyDistanceMatrix:
     def __init__(self, data):
-        self.data = data  # 原始数据点
-        self.clusters = [[i] for i in range(len(data))]  # 每个样本初始为一个簇
-        self.cache = {}  # 距离缓存，避免重复计算
-
+        self.data = data 
+        self.clusters = [[i] for i in range(len(data))] 
+        self.cache = {} 
+        
     def get(self, i, j):
         if i == j:
-            return np.inf  # 防止合并自己
+            return np.inf  
         key = (min(i, j), max(i, j))
         if key in self.cache:
             return self.cache[key]
 
-        # 计算簇 i 与簇 j 中最小距离（Single-Linkage）
+      
         min_dist = math.inf
         for idx1 in self.clusters[i]:
             for idx2 in self.clusters[j]:
@@ -26,22 +26,21 @@ class LazyDistanceMatrix:
         return min_dist
 
     def update_merge(self, i, j):
-        # 合并簇 j 到 i
         self.clusters[i].extend(self.clusters[j])
         del self.clusters[j]
 
-        # 更新缓存：簇 j 被移除，i 保留，更新与 i 的最小距离
+       
         new_cache = {}
         for key in self.cache:
             a, b = key
             if j in key:
-                continue  # 删除与 j 相关的项
+                continue  
             if a > j: a -= 1
             if b > j: b -= 1
             new_cache[(a, b) if a < b else (b, a)] = self.cache[key]
         self.cache = new_cache
 
-        # 重建 i 的距离
+      
         for k in range(len(self.clusters)):
             if k == i:
                 continue
@@ -49,11 +48,11 @@ class LazyDistanceMatrix:
             self.cache[(min(i, k), max(i, k))] = dist
 
     def _min_dist_between_clusters(self, i, j):
-        # 提供内部更新最小距离函数
         min_dist = math.inf
         for idx1 in self.clusters[i]:
             for idx2 in self.clusters[j]:
                 dist = minkowski(self.data[idx1], self.data[idx2])
                 if dist < min_dist:
                     min_dist = dist
+
         return min_dist
